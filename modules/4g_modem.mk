fdfbc46884d55134e9fafbe1556985c1acc8f5e6
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-03-31 19:09:30 +0530

    common: Add support for GOSUNCN LTE module
    Change-Id: I132692dc00e6929bdb8453be5efb800d077dae75
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- modules/4g_modem.mk
+++ modules/4g_modem.mk
@@ -18,10 +18,32 @@ PRODUCT_PACKAGES += \
     CarrierDefaultApp \
     CarrierConfig \
     rild \
+    libril \
+    libreference-ril \
+    usb_dongle \
+    radio_dependencies \
     librk-ril \
     dhcpcd
 
 PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
+EXTRA_VENDOR_LIBRARIES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio@1.6 \
+    android.hardware.radio.config@1.0-service \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.config@1.3 \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.1 \
+    android.hardware.secure_element@1.2
 
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2-radio-service \
@@ -29,22 +51,30 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.noril=false \
-    ro.telephony.default_network=9
+    ro.radio.noril=false \
+    ro.telephony.default_network=26 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.radio.multisim.config=ssss \
+    persist.vendor.radio.rat_on=combine
 
 DEVICE_MANIFEST_FILE += device/rockchip/common/4g_modem/manifest.xml
+DEVICE_MATRIX_FILE += device/rockchip/common/4g_modem/compatibility_matrix.xml
 
 ifeq ($(strip $(TARGET_ARCH)), arm64)
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.rild.libpath=/vendor/lib64/librk-ril.so
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     vendor.rild.libpath=/vendor/lib64/librk-ril.so
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/4g_modem/bin64/dhcpcd:$(TARGET_COPY_OUT_VENDOR)/bin/dhcpcd \
-    $(LOCAL_PATH)/4g_modem/lib64/librk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librk-ril.so
+    $(LOCAL_PATH)/../4g_modem/bin64/dhcpcd:$(TARGET_COPY_OUT_VENDOR)/bin/dhcpcd \
+    $(LOCAL_PATH)/../4g_modem/lib64/librk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librk-ril.so
 else
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.rild.libpath=/vendor/lib/librk-ril.so
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/4g_modem/bin32/dhcpcd:$(TARGET_COPY_OUT_VENDOR)/bin/dhcpcd \
-    $(LOCAL_PATH)/4g_modem/lib32/librk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/librk-ril.so
+    $(LOCAL_PATH)/../4g_modem/bin32/dhcpcd:$(TARGET_COPY_OUT_VENDOR)/bin/dhcpcd \
+    $(LOCAL_PATH)/../4g_modem/lib32/librk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/librk-ril.so
 endif
